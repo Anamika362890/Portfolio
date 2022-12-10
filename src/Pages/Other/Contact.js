6dad93eb17cd4652f19e876973c29f114b61bf1a
unknown <anamika362890@gmail.com>
2022-12-10 23:56:41 +0600

    contact

--- src/Pages/Other/Contact.js
+++ src/Pages/Other/Contact.js
@@ -4,20 +4,23 @@ import { Form } from 'react-router-dom';
 const Contact = () => {
     return (
 
-        <div className='App text-white'>
-            <h1>Contact Me</h1>
+        <div className='App text-white my-20'>
+            <h1 className='text-success text-4xl font-bold my-4'>Contact Me</h1>
             <p>Please do not hesitate to contact me
                 , I will  try to give you my best.</p>
-            <h1>Address : Dhaka,Bangladesh</h1>
-            <h1>Email : Anamika362890@gmail.com</h1>
-            <h1>OR</h1>
+            <div className="badge badge-success badge-outline my-3"> <h1 className='text-lg '>Address : Dhaka,Bangladesh</h1></div> <br></br>
+            <div className="badge badge-success badge-outline"> <h1 className='text-lg'>Email : Anamika362890@gmail.com</h1></div>
 
 
-            <input type="text" placeholder="Type here" className=" mb-5 input input-bordered w-full max-w-xs" /> <br></br>
-            <input type="text" placeholder="Type here" className="input mb-5 input-bordered w-full max-w-xs" /><br></br>
-            <textarea className="textarea textarea-bordered w-full max-w-xs" placeholder="Bio"></textarea>
+
+            <h1 className='text-3xl my-4'>OR</h1>
+
+
+            <input type="text" placeholder="Enter Your Name" className=" mb-5 input input-bordered w-full max-w-xl" required /> <br></br>
+            <input type="email" placeholder="Enter Your Email" className="input mb-5 input-bordered w-full max-w-xl" required /><br></br>
+            <textarea className="textarea textarea-bordered w-full pb-8 max-w-xl" placeholder="Message"></textarea>
             <br></br>
-            <button className='btn'>Send Message</button>
+            <button className='btn btn-success text-white mt-9'>Send Message</button>
 
 
 
